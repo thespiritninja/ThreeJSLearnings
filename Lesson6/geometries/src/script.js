cbb0cd62460d3d464bc4d882f8202133c30b42bf
Shreyas Sawant <shreyassawant018@gmail.com>
2024-03-07 14:01:51 -0500

    Lesson 6 completed

---
+++ Lesson6/geometries/src/script.js
@@ -0,0 +1,107 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Object
+ */
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+//Creating a custom geometry 
+const positionsArray = new Float32Array([
+    0,0,0,
+    0,1,0,
+    0,0,1
+])
+const positionAttribute = new THREE.BufferAttribute(positionsArray,3)
+const geometry = new THREE.BufferGeometry()
+geometry.setAttribute('position', positionAttribute)
+const material = new THREE.MeshBasicMaterial({ color: '#9999ff', wireframe:true })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+//Pixel Ratio
+renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+window.addEventListener('resize',()=>{
+    //update window sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    //update camera
+    camera.aspect = (sizes.width/sizes.height)
+    camera.updateProjectionMatrix()
+
+    
+    //Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+
+    //update orbitController
+    controls.update()
+})
+
+window.addEventListener('dblclick',()=>{
+    const fullScreenDevice = document.fullscreenElement || document.webkitFullscreenElement
+
+    if(!fullScreenDevice){
+        canvas.requestFullscreen ? canvas.requestFullscreen() : canvas.webkitRequestFullscreen()
+    }
+    else{
+        document.exitFullscreen ? document.exitFullscreen() : document.webkitExitFullScreen()
+    }
+})
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
